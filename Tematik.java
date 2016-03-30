685371768d24b48df5c2c42e08673719ec6bfa1a
SyifaNF12 <syifanurfauziah472@gmail.com>
2016-03-30 16:04:34 +0700

    tugas

---
+++ Tematik.java
@@ -0,0 +1,20 @@
+public class Tematik {
+
+public static void main (String[] args){
+
+System.out.println("Syifa Nur Fauziah");
+System.out.println("Sekolah Tinggi Teknologi Bandung");
+
+int a, b, c, d, e, f;
+a = 4;
+b = 2;
+c = a - b;
+d = a + b;
+e = a * b;
+f = a / b;
+System.out.println("Hasil Pengurangannya adalah :" +c);
+System.out.println("Hasil Pertambahannya adalah :" +d);
+System.out.println("Hasil Perkaliannya adalah :" +e);
+System.out.println("Hasil Pembagiannya adalah :" +f);
+}
+}

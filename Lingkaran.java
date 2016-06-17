599674ea5dcb0222489b97e58a29252d982d83dc
Syifa Nur Fauziah <syifanurfauziah472@gmail.com>
2016-06-17 15:11:27 +0700

    Create Lingkaran.java

---
+++ Lingkaran.java
@@ -0,0 +1,38 @@
+package bangundatar;
+
+public class Lingkaran implements BangunDatar {
+    // Deklarasi atribut
+  private final double radius;
+  private final double PI = 3.14;
+
+  // Definisi method
+  // Konstruktor
+  public Lingkaran(double r) {
+    radius = r;
+  }
+
+  // Selector
+  public double getRadius() {
+    return radius;
+  }
+
+  // Definisi method dari kelas induk
+  @Override
+  public double Luas() {
+    return (PI*radius*radius);
+  }
+
+  @Override
+  public double Keliling() {
+    return (2*PI*radius);
+  }
+  
+  @Override
+  public String displayInfo() {
+    return ( "Lingkaran"
+           + "\no Jari-jari: " + (float)getRadius()
+           + "\no Luas     : " + (float)Luas()
+           + "\no Keliling : " + (float)Keliling() );
+  }
+   
+}
